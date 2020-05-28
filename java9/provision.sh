f24604ba3443955c4b763efd9277709464812727
Chris Runo <chris.runo@gmail.com>
2020-05-28 13:52:56 -0400

    1: Remove aggreement.

--- java9/provision.sh
+++ java9/provision.sh
@@ -4,16 +4,10 @@ if [[ -f /usr/bin/java ]]; then
 	exit 0
 fi
 
-echo "Installing Java 9"
+echo "Installing JRE & JDK"
 
 # Install prerequisites.
 sudo apt-get install -y python-software-properties debconf-utils &>/dev/null
-
-# Licensing.
-echo "Installing this utility means you have AGREED to the Oracle Binary Code"
-echo "License Agreement for Java SE at"
-echo "http://www.oracle.com/technetwork/java/javase/terms/license/index.html"
-echo "oracle-java9-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 
 # Install Java.
 sudo apt-get install -y default-jre default-jdk
